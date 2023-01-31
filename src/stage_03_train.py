8d49ae10ff5d4599354a50bf6296fd17a1d79557
Techn08055 <technomadlyf@gmail.com>
2023-01-31 07:40:07 +0530

    stage_03_updated

--- src/stage_03_train.py
+++ src/stage_03_train.py
@@ -5,9 +5,11 @@ from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
 import random
+import joblib
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
-
-STAGE = "STAGE_NAME" ## <<< change stage name 
+STAGE = "THREE" ## <<< change stage name 
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -21,9 +23,33 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+    
+    artifacts = config["artifacts"]
+    featurized_data_dir = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["FEATURIZED_DATA"])    
+    featurized_train_data_path = os.path.join(featurized_data_dir,artifacts["FEATURIZED_OUT_TRAIN"])
+    
+    model_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["MODEL_DIR"])
+    create_directories([model_dir_path])
+    model_path = os.path.join(model_dir_path, artifacts['MODEL_NAME'])
 
+    matrix = joblib.load(featurized_train_data_path)
+    labels = np.squeeze(matrix[:,1].toarray())
+    X = matrix[:,2:]
+    
+    seed = params["train"]["seed"]
+    n_est = params ["train"]["n_est"]
+    min_splits = params ["train"]["min_split"]
 
+    model = RandomForestClassifier(
+        n_estimators= n_est,
+        min_samples_split= min_splits,
+        n_jobs= 2,
+        random_state= seed
+    )
+
+    model.fit(X, labels)
+    joblib.dump(model, model_path)
+    
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
